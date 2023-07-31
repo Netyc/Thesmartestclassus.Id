a4c5d66e6f8023e1bbeaf971daa8c72aebe436b6
Fallizana <126937820+Fal-code@users.noreply.github.com>
2023-07-31 21:39:43 +0700

    Add files via upload

---
+++ Foto/script.js
@@ -0,0 +1,41 @@
+let navbar = document.querySelector('.header .navbar');
+document.querySelector('#menu-btn').onclick = () =>{
+  navbar.classList.toggle('active');
+
+}
+
+window.onscroll = () =>{
+  navbar.classList.remove('active');
+}
+
+
+const typed = new Typed('.multiple-text', {
+    strings: ['cerdas', 'kreative', 'berwawasan tinggi', 'Sekolah Unggul Berbasis Karakter'],
+    typeSpeed: 100,
+    backSpeed: 100,
+    backDelay: 1000,
+    loop: true
+});
+
+ const filterContainer = document.querySelector(".gallery-filter"),
+ galleryItems = document.querySelectorAll(".gallery-item");
+
+ filterContainer.addEventListener("click", (event) =>{
+   if(event.target.classList.contains("filter-item")){
+   	 // deactivate existing active 'filter-item'
+   	 filterContainer.querySelector(".active").classList.remove("active");
+   	 // activate new 'filter-item'
+   	 event.target.classList.add("active");
+   	 const filterValue = event.target.getAttribute("data-filter");
+   	 galleryItems.forEach((item) =>{
+       if(item.classList.contains(filterValue) || filterValue === 'all'){
+       	item.classList.remove("hide");
+       	 item.classList.add("show");
+       }
+       else{
+       	item.classList.remove("show");
+       	item.classList.add("hide");
+       }
+   	 });
+   }
+ });
